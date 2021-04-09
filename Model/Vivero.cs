4ff16004f989c208c8aa685ebbcf58d17055200f
Allan Oviedo <allan5kzx1@hotmail.com>
2021-04-09 17:11:18 -0600

    firts

---
+++ Model/Vivero.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Vivero_Prueba.Model
+{
+    public class Vivero
+    {
+        public string id_planta { get; set; }
+        public string nombre_planta { get; set; }
+        public string tipo_planta { get; set; }
+       
+    }
+}
